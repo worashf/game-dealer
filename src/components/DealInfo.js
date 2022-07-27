87daf3d16c3edfa8f3a8a140fdc7f708cc146dea
worashf <uppert83@gmail.com>
2022-07-27 19:24:09 +0300

    add deal info

---
+++ src/components/DealInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DealInfo = () => (
+  <div className="info">
+    <p>
+      Best Computer Games,
+      <p> Deals </p>
+    </p>
+  </div>
+);
+
+export default DealInfo;
