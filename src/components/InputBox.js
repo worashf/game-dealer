2e99e5f410d0abaa167d1075e029c00ae692e4a4
worashf <uppert83@gmail.com>
2022-07-27 19:34:14 +0300

    Add search deal by title input

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const InputBox = () => {
-  return <>Hello input</>;
-};
+const InputBox = () => (
+  <div className="input-box">
+    <input
+      className="input"
+      type="text"
+      name="city-name"
+      placeholder="Enter Game Title"
+    />
+  </div>
+);
 
 export default InputBox;
