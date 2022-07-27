2ead171b8295f3f898ff4fe991b70fc4950e6053
worashf <uppert83@gmail.com>
2022-07-27 19:35:03 +0300

    add home page

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import DealInfo from '../components/DealInfo';
+import DealList from '../components/DealList';
 
-const Home = () => {
-  return <>Hell home</>;
-};
+const Home = () => (
+  <>
+    <DealInfo />
+    <DealList />
+  </>
+);
 
 export default Home;
