b5e6d1d1a2e2d44734a378f533f4d262b6c20c17
worashf <uppert83@gmail.com>
2022-07-27 19:34:49 +0300

    add header nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BiMicrophone, BiCog } from 'react-icons/bi';
+import { FaChevronLeft } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
-  return <>Hello NavBar</>;
-};
+import './Navbar.css';
+
+const NavBar = () => (
+  <div className="header">
+    <NavLink to="/">
+      {' '}
+      <FaChevronLeft className="logo" />
+    </NavLink>
+
+    <h3 className="title"> Games Deals Finder</h3>
+    <div className="icons">
+      <BiMicrophone className="icon" />
+      <BiCog className="icon" />
+    </div>
+  </div>
+);
 
 export default NavBar;
