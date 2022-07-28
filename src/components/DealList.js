d75c41bf9b6f4febe98226c278b873839050e3c9
worashf <uppert83@gmail.com>
2022-07-28 16:49:47 +0300

    add search input and render condition  for search

--- src/components/DealList.js
+++ src/components/DealList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Deal from './Deal';
@@ -7,6 +7,7 @@ import { getGameDeals } from '../redux/GameDeals/GameDeals';
 import Spinner from './Spinner';
 
 const DealList = () => {
+  const [title, setTitle] = useState('');
   const { deals, loading } = useSelector((state) => state.deals);
   const dispatch = useDispatch();
   console.log(deals, 'deal');
@@ -22,21 +23,40 @@ const DealList = () => {
         <Spinner />
       ) : (
         <div>
-          {' '}
-          <InputBox />
+          <div className="input-box">
+            <input
+              value={title}
+              className="input"
+              type="text"
+              name="city-name"
+              placeholder="Enter Game Title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <InputBox />
+          </div>
           <div className="deal-container">
-            {deals.map((deal) => (
-              <Link to={`detail/${deal.id}`} key={deal.id}>
-                <Deal
-                  key={deal.id}
-                  thumb={deal.thumb}
-                  title={deal.title}
-                  salePrice={deal.salePrice}
-                  normalPrice={deal.normalPrice}
-                />
-                {' '}
-              </Link>
-            ))}
+            {deals.length !== 0 ? (
+              deals
+                .filter((del) => del.title.match(title))
+                .map((deal) => (
+                  <Link
+                    to={`detail/${deal.id}`}
+                    key={deal.id}
+                    className="deals"
+                  >
+                    <Deal
+                      key={deal.id}
+                      thumb={deal.thumb}
+                      title={deal.title}
+                      salePrice={deal.salePrice}
+                      normalPrice={deal.normalPrice}
+                    />
+                    {' '}
+                  </Link>
+                ))
+            ) : (
+              <div style={{ color: 'red' }}> no Data</div>
+            )}
           </div>
         </div>
       )}
